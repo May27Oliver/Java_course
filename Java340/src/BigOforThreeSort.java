30f3a4bc17f15461cc501b8208d2ffe531aa6fdf
oliver310338 <oliver310338@hotmail.com>
2021-03-25 11:57:15 +0800

    Big O ms ns

--- Java340/src/BigOforThreeSort.java
+++ Java340/src/BigOforThreeSort.java
@@ -2,21 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BigOforThreeSort {
-
 	public static void main(String[] args) {
-		//開始測量演算法時間：
-		// n=1000;
-		System.out.printf("\t%s\t","Size");
-		System.out.printf("\t%s\t","Bubble sort");
-		System.out.printf("\t%s\t","Selection sort");
-		System.out.printf("\t%s\t","Insertion sort");
-		System.out.printf("\t%s\t\n","Arrays.sort ");
-		
 		int [] testScale = {1000,2000,4000,8000,16000,32000,64000};
-		for (int i = 0; i < testScale.length ; i++) {
-			int[] a = new generateArray().arrayUWant(testScale[i]);
-			new TestTime().Test(a);
-		}
+		TestTime startTest = new TestTime();
+		startTest.beginMsTest(testScale);
+		startTest.beginNsTest(testScale);
 	}
 }
 
@@ -27,8 +17,10 @@ public class BigOforThreeSort {
  改：set(index,element);
  查：get(index); subList(from,to); listIterator();
  */
+
+//產生陣列
 class generateArray{
-	static int[] arrayUWant(int n) {
+	int[] arrayUWant(int n) {
 		int[] a= new int[n];
 		for(int i = 0 ; i < a.length ; i++) {
 			a[i] = ((int) (Math.random()*n));
@@ -39,48 +31,108 @@ class generateArray{
 	}
 }
 
+//測試方法
 class TestTime{
-	static void Test(int[] arr) {
+	//毫秒測速
+	void beginMsTest(int[] a){
+		//開始測量演算法時間：
+		System.out.printf("\t單位（%s）%n","ms");
+		System.out.printf("\t%s\t","Size");
+		System.out.printf("\t%s\t","Bubble sort");
+		System.out.printf("\t%s\t","Selection sort");
+		System.out.printf("\t%s\t","Insertion sort");
+		System.out.printf("\t%s\t\n","Arrays.sort ");
+
+		for (int i = 0; i < a.length ; i++) {
+			int[] arr = new generateArray().arrayUWant(a[i]);
+			msTest(arr);
+		}
+	}
+
+	void beginNsTest(int[] a){
+		//開始測量演算法時間：
+		System.out.printf("\t單位（%s）%n","ns");
+		System.out.printf("\t%s\t","Size");
+		System.out.printf("\t%s\t","Bubble sort");
+		System.out.printf("\t%s\t","Selection sort");
+		System.out.printf("\t%s\t","Insertion sort");
+		System.out.printf("\t%s\t\n","Arrays.sort ");
+
+		for (int i = 0; i < a.length ; i++) {
+			int[] arr = new generateArray().arrayUWant(a[i]);
+			nsTest(arr);
+		}
+	}
+
+	void msTest(int[] arr) {
 		//陣列大小
-		System.out.printf("\t%d\t",arr.length);
-		
-		//Bobble Sort
-		double startTime = System.currentTimeMillis(); //獲取開始時間
+		System.out.printf("\t%d\t\t",arr.length);
+		//Bubble Sort
+		long startTime = System.currentTimeMillis();
 		new Sort().BubbleSort(arr);
-		double endTime = System.currentTimeMillis(); //獲取結束時間
-		System.out.printf("%f\t",endTime - startTime);
+		long endTime = System.currentTimeMillis();
+		System.out.printf("%d\t\t\t\t",endTime - startTime);
 		
 		//Select Sort
-		startTime = System.currentTimeMillis(); //獲取開始時間
+		long startSelectTime = System.currentTimeMillis();
 		new Sort().SelectSort(arr);
-		endTime = System.currentTimeMillis(); //獲取結束時間
-		System.out.printf("\t%f\t",endTime - startTime);
+		long endSelectTime = System.currentTimeMillis();
+		System.out.printf("\t%d\t\t\t",endSelectTime - startSelectTime);
 		
 		//Insert sort
-		startTime = System.currentTimeMillis(); //獲取開始時間
+		long startInsertTime = System.currentTimeMillis();
 		new Sort().InsertSort(arr);
-		endTime = System.currentTimeMillis(); //獲取結束時間
-		System.out.printf("\t%f\t\t",endTime - startTime);
-		
+		long endInsertTime = System.currentTimeMillis();
+		System.out.printf("\t%d\t\t\t\t",endInsertTime - startInsertTime);
+
 		//Arrays sort
-		startTime = System.currentTimeMillis(); //獲取開始時間
+		long startArraysTime = System.currentTimeMillis();
 		Arrays.sort(arr);
-		endTime = System.currentTimeMillis(); //獲取結束時間
-		System.out.printf("\t%f\t\n",endTime - startTime);
+		long endArraysTime = System.currentTimeMillis();
+		System.out.printf("\t%d\t\n", endArraysTime - startArraysTime);
+	}
+	//奈秒測速
+	void nsTest(int[] arr) {
+		//陣列大小
+		System.out.printf("\t%d\t",arr.length);
+		//Bubble Sort
+		long startTime = System.nanoTime(); //獲取開始時間
+		new Sort().BubbleSort(arr);
+		long endTime = System.nanoTime(); //獲取結束時間
+		System.out.printf("\t%d\t\t",endTime - startTime);
+
+		//Select Sort
+		long startSelectTime = System.nanoTime(); //獲取開始時間
+		new Sort().SelectSort(arr);
+		long endSelectTime = System.nanoTime(); //獲取結束時間
+		System.out.printf("\t%d\t\t",endSelectTime - startSelectTime);
+
+		//Insert sort
+		long startInsertTime = System.nanoTime(); //獲取開始時間
+		new Sort().InsertSort(arr);
+		long endInsertTime = System.nanoTime(); //獲取結束時間
+		System.out.printf("\t\t%d\t\t",endInsertTime - startInsertTime);
+
+		//Arrays sort
+		long startArraysTime = System.nanoTime(); //獲取開始時間
+		Arrays.sort(arr);
+		long endArraysTime = System.nanoTime(); //獲取結束時間
+		System.out.printf("\t\t%d\t\n",endArraysTime - startArraysTime);
 	}
 }
 
+//三種排序法
 class Sort {
 	//	氣泡排序法
-	static int[] BubbleSort (int[] arr) {
+	int[] BubbleSort (int[] arr) {
 			int n = arr.length;
-			int temp = 0;
+			int temp;
 			for (int i = 0 ; i < n ;i++) {
 				for (int j = 1; j < ( n - i ); j++) {
 					if( arr[ j - 1 ] > arr[ j ] ) {
-							temp = arr[ j - 1 ];
-							arr[ j - 1 ] = arr[ j ];
-							arr[ j ] = temp;
+						temp = arr[ j - 1 ];
+						arr[ j - 1 ] = arr[ j ];
+						arr[ j ] = temp;
 					}
 				}
 			}
@@ -88,7 +140,7 @@ class Sort {
 	}
 	
 	//	選擇排序法
-	static int[] SelectSort( int[] arr) {
+	int[] SelectSort( int[] arr) {
 		int min;
 		for( int i = 0; i < arr.length ; i++) {
 			int m = i;//假設最小值索引是i
@@ -110,7 +162,7 @@ class Sort {
 	}
 	
 	//	插入排序法
-	static int[] InsertSort( int[] arr) {
+	int[] InsertSort( int[] arr) {
 		int temp;
 		for (int i = 1 ; i < arr.length ; i++) {
 			temp = arr[i];
@@ -125,3 +177,8 @@ class Sort {
 		return arr;
 	}
 }
+
+/*
+* 問題一，static在class內可以省略？
+* 問題二，nsTest和msTest內有太多重複的程式碼，不知有無方法精簡
+* */
