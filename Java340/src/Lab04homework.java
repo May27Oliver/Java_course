14df490b17b231f56145d4c6e0d3f8c553bd99a9
oliver310338 <oliver310338@hotmail.com>
2021-03-30 12:20:48 +0800

    recursion note

--- Java340/src/Lab04homework.java
+++ Java340/src/Lab04homework.java
@@ -1,17 +1,24 @@
 public class Lab04homework {
     public static void main(String[] args){
+        //x^n
+        int x = 2;
+        int n = 60;
+
+        //test speed O(logN)
         System.out.printf("%10s%20s%n" , "O(logN) ","O(N) ");
         double t0 = System.nanoTime() / 1e6;
-        seperatePow(2,10);//x ^ n
+        seperatePow(x,n);//x ^ n
         double t1 = System.nanoTime() / 1e6;
         System.out.printf("%10f", t1 - t0 );
 
+        //test speed O(N)
         t0 = System.nanoTime() / 1e6;
-        countPow(2,10);
+        countPow(x,n);
         t1 = System.nanoTime() / 1e6;
         System.out.printf("%20f", t1 - t0 );
     }
 
+    //O(N)
     public static long countPow(long x, int n){
         if(n == 1){
             return x;
@@ -20,6 +27,7 @@ public class Lab04homework {
         }
     }
 
+    //O(logN)
     public static long beginner = 1;
     public static long seperatePow(long x, int n){
 
